ba74fe57d852ed24285c2fbc4d6d96b400294a96
juliankopkalarsen <juliankopkalarsen@gmail.com>
2015-05-11 14:34:53 +0200

    added testing infrastructure for vectors

--- GMM/src/Math/Test.hs
+++ GMM/src/Math/Test.hs
@@ -18,7 +18,10 @@ import Test.Framework (testGroup, Test)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck (testProperty)
 import Test.HUnit hiding (Test)
+import Test.QuickCheck
+
 import Numeric.LinearAlgebra (Vector, fromList, toRows)
+import Foreign.Storable (Storable)
 
 import Math
 
@@ -26,8 +29,16 @@ mathSuite :: Test
 mathSuite = testGroup "Auxillary math functions"
     [testCase "The mean of a vector is the vector itself" (testMean [(fromList [1.0]), (fromList [1.0])] (fromList [1.0]) ),
      --testCase "should fail" (testMean [(fromList [1.0]), (fromList [2.0])] (fromList [1.0]) ),
-     testCase "The mean of a longer vector is the vector itself" (testMean [(fromList [1.0, 2.0]), (fromList [1.0, 2.0])] (fromList [1.0, 2.0]) )
+     testProperty "The mean of a vector is the vector itself" meanProp1
      ]
 
 testMean :: [Vector Double] -> Vector Double -> Assertion
 testMean points expected = expected @=? (head . toRows . mean) points
+
+instance (Arbitrary a, Storable a) => Arbitrary (Vector a) where
+    arbitrary = fmap fromList arbitrary
+
+meanProp1 x = x == x
+        where types = (x::Vector Double)
+
+
